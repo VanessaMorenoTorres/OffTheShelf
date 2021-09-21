0863098b2e1e96f19db47e11867fc8cd528f35c6
VanessaMorenoTorres <morenot.vanessa@gmail.com>
2021-09-21 10:10:53 -0500

    first api now receives data

--- off-the-shelf/src/components/Books.js
+++ off-the-shelf/src/components/Books.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import SearchArea from "./SearchArea";
-
+import request from 'superagent';
 
 class Books extends Component {
   constructor(props) {
@@ -8,31 +8,29 @@ class Books extends Component {
     this.state = {
       books: [],
       searchField: "",
-    };
+    }
   }
 
-
-  handleSearch = (e) => {
-    // console.log(e.target.value) <--this worked, state is changing as user types
-    this.setState({searchField: e.target.value })
-  }
   searchBook = (e) =>{
     e.preventDefault();
-    console.log(e.target.value)
-    let searchField = e.target.value
-    // fetch(`https://www.googleapis.com/books/v1/volumes?q=search-terms&key=your-API-key`)
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchField}`)
-      .then(response => response.json())
-      .then(json => console.log(json))}
-  //   .then(result => {
-  // this.setState({ books: Books.books})
+    request
+    .get('https://www.googleapis.com/books/v1/volumes')
+    .query({q: this.state.searchField})
+    .then((data)=> {
+      console.log(data);
+    })
+  }
 
-  
+  handleSearch = (e) => {
+    console.log(e.target.value) 
+    // ^this worked, state is changing as user types
+    this.setState({searchField: e.target.value })
+  }
 
   render() {
     return (
       <div className="books">
-        <SearchArea searchBook={this.handleSearch} handleSearch={this.handleSearch} />
+        <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} />
         <p>Showing results for:</p>
       
       </div>
@@ -40,3 +38,48 @@ class Books extends Component {
   };
 }
 export default Books;
+
+
+
+// below is original attempt to get api but it did not work, trying alternative way above 
+// class Books extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       books: [],
+//       searchField: "",
+//     };
+//   }
+
+
+//   handleSearch = (e) => {
+//     console.log(e.target.value) 
+//     // ^this worked, state is changing as user types
+//     this.setState({searchField: e.target.value })
+//   }
+//   searchBook = (e) =>{
+//     e.preventDefault();
+//     // console.log(e.target.value)
+//     // let searchField = e.target.value
+//     let searchField = Books.searchField.value
+//     console.log(e.target.value)
+//     // fetch(`https://www.googleapis.com/books/v1/volumes?q=search-terms&key=your-API-key`)
+//     fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchField}`)
+//       .then(response => response.json())
+//       .then(data => console.log(data))}
+//   //   .then(result => {
+//   // this.setState({ books: Books.books})
+
+  
+
+//   render() {
+//     return (
+//       <div className="books">
+//         <SearchArea searchBook={this.handleSearch} handleSearch={this.handleSearch} />
+//         <p>Showing results for:</p>
+      
+//       </div>
+//     );
+//   };
+// }
+// export default Books;
