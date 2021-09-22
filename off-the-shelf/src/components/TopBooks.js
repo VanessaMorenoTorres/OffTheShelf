faa8ad5aedf5e0a792c8984fb09bc7ec1598d083
VanessaMorenoTorres <morenot.vanessa@gmail.com>
2021-09-22 09:49:16 -0500

    nyTimes api works, data now displays

--- off-the-shelf/src/components/TopBooks.js
+++ off-the-shelf/src/components/TopBooks.js
@@ -7,9 +7,13 @@ function TopBooks(){
         fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`)
         .then(response => response.json())
         .then(data => 
-            // setTop(data.data.results.books),
-            console.log(data.results.books))
+            setTop(data.results.books),
+            console.log(setTop)
+            )
+            // console.log(setTop)
+            // console.log(data.results.books)) this worked data appeared 
     }, [])
+
 
 
 
@@ -17,9 +21,24 @@ function TopBooks(){
         <div>
             <h3>Here is where columns will appear</h3>
             <section>
-                {/* {books.map((top) => {
-                    const {} = top
-                })} */}
+                {top.map((top) => {
+                    const {rank, description, title, author, primary_isbn10, book_image, amazon_product_url} = top
+                return(
+                    <article key={rank}>
+                        <div>
+                            <img src={book_image} alt={title} />
+                        </div>
+                        <div>
+                            <h3>Rank: {rank}</h3>
+                            <h3>Title: {title}</h3>
+                            <h4>Author: {author}</h4>
+                            <p>Description: {description}</p>
+                            <p>ISBN: {primary_isbn10}</p>
+                            <a href={amazon_product_url}>Buy Now</a>
+                        </div>
+                    </article>
+                )
+                })}
             </section>
         </div>
     )
